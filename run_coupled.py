ee47c4b19aff9f54ee55237545271b44696ea93a
Sam Friedman <samtx@outlook.com>
2016-12-31 14:57:11 -0600

    making progress combining all OpenAeroStruct framework into one python file... still not working all the way

--- run_coupled.py
+++ run_coupled.py
@@ -2,27 +2,40 @@
 
 from __future__ import print_function
 import coupled
+import aerostruct
 import warnings
 import sys
 
-warnings.filterwarnings("ignore") # to disable openmdao warnings which will create an error in Matlab
+# to disable openmdao warnings which will create an error in Matlab
+warnings.filterwarnings("ignore")
+
 
 def main(num_inboard=2, num_outboard=3, check=False):
 
-    print('\nRun setup()...')
+    print('\nRun coupled.setup()...')
     def_mesh, params = coupled.setup(num_inboard, num_outboard, check)
-    print('def_mesh...  def_mesh.shape =',def_mesh.shape)
+    print('def_mesh...  def_mesh.shape =', def_mesh.shape)
     print(def_mesh)
 
-    print('\nRun aero()...')
+    print('\nRun aerostruct.setup()...')
+    def_mesh, params = aerostruct.setup(num_inboard, num_outboard)
+    print('def_mesh...  def_mesh.shape =', def_mesh.shape)
+    print(def_mesh)
+
+    print('\nRun coupled.aero()...')
     loads = coupled.aero(def_mesh, params)
-    print('loads matrix... loads.shape =',loads.shape)
+    print('loads matrix... loads.shape =', loads.shape)
     print(loads)
 
-    print('\nRun struct()...')
-    def_mesh = coupled.struct(loads, params)
-    print('def_mesh...  def_mesh.shape =',def_mesh.shape)
-    print(def_mesh)
+    print('\nRun aerostruct.aero()...')
+    loads = aerostruct.aero(def_mesh, params)
+    print('loads matrix... loads.shape =', loads.shape)
+    print(loads)
+
+    # print('\nRun struct()...')
+    # def_mesh = coupled.struct(loads, params)
+    # print('def_mesh...  def_mesh.shape =',def_mesh.shape)
+    # print(def_mesh)
 
 if __name__ == '__main__':
     main()
