1780bd51a7307340eaab5ef7355361329c07493e
Sam Friedman <samtx@outlook.com>
2017-01-01 22:20:59 -0600

    begin cythonizing aerostruct.py

---
+++ setup_aerostruct_cython.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from Cython.Build import cythonize
+import numpy
+
+setup(
+	ext_modules = cythonize("aerostruct_cython.pyx"),
+	include_dirs=[numpy.get_include()]
+)
+
+# Run this from the Anaconda prompt to compile Cython code...
+# $ python setup_aerostruct_cython.py build_ext --inplace
