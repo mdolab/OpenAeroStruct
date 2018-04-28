9956196afdfddb78f9b84d7a645b286d1dc4b828
Sam Friedman <samtx@outlook.com>
2018-04-28 18:22:00 -0500

    Update mat2np.m and np2mat.m for N-dim array conversions

--- matlab/np2mat.m
+++ matlab/np2mat.m
@@ -1,8 +1,29 @@
 function mat = np2mat(npary)
 
-% convert python (Numpy) ndarray to matlab
-sh = double(py.array.array('d',npary.shape));
-npary2 = double(py.array.array('d',py.numpy.nditer(npary)));
-mat = reshape(npary2,fliplr(sh))';  % matlab 2d array 
+% with help from: https://www.mathworks.com/matlabcentral/answers/157347-convert-python-numpy-array-to-double
 
+% convert python (Numpy) ndarray to matlab
+try
+  % if scalar
+  mat = double(npary);
+  % disp('A');
+catch
+  % if array
+  sh = cellfun(@int64,cell(npary.shape));
+  if length(sh) == 1
+      % if a numpy 1D flattened array
+      mat = double(py.array.array('d',py.numpy.nditer(npary)));
+  else if length(sh) == 2
+      % if a numpy 2D array
+      npary2 = double(py.array.array('d',py.numpy.nditer(npary)));
+      mat = reshape(npary2,fliplr(sh))';  % matlab 2d array
+  else if length(sh) > 2
+     % if a numpy 3D or higher dimension array
+     npary3 = double(py.array.array('d',py.numpy.nditer(npary, pyargs('order','C'))));
+     % fprintf('sh=%f\n',sh);
+     mat = reshape(npary3, fliplr(sh));
+     mat = permute(mat, [length(sh):-1:1]);  % matlab Nd array, N >= 3
+  end
+end
+end
 end
