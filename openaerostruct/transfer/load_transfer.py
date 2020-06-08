b950c2fe158ec04b2157b7ab8095e9b7400dd8b7
kanekosh <shugok@umich.edu>
2020-06-07 22:47:03 -0400

    fixed partials in load_transfer

--- openaerostruct/transfer/load_transfer.py
+++ openaerostruct/transfer/load_transfer.py
@@ -16,6 +16,7 @@ class LoadTransfer(om.ExplicitComponent):
     ----------
     def_mesh[nx, ny, 3] : numpy array
         Array defining the lifting surfaces after deformation.
+        Arrays will be flattened in Fortran order (only relevant when more than one chordwise panel).
     sec_forces[nx-1, ny-1, 3] : numpy array
         Array containing the sectional forces acting on each panel.
 
@@ -168,55 +169,70 @@ class LoadTransfer(om.ExplicitComponent):
             + 0.5 * w1 * mesh[1:, 1:, :]
         )
 
-        # Compute the structural midpoints based on the fem_origin location
-        s_pts = (
-            0.5 * (1 - w2) * mesh[0, :-1, :]
-            + 0.5 * w2 * mesh[-1, :-1, :]
-            + 0.5 * (1 - w2) * mesh[0, 1:, :]
-            + 0.5 * w2 * mesh[-1, 1:, :]
-        )
+        # Compute the structural nodes
+        s_pts = (1 - self.w2) * mesh[0, :, :] + self.w2 * mesh[-1, :, :]
 
-        diff = 0.5 * (a_pts - s_pts)
+        # ----- 1. dmoment__dsec_forces -----
+        # Sensitivity of loads (moments) at inner node wrt sec_force
+        diff_in = 0.5 * (a_pts - s_pts[:-1, :])  # moment arm from inner node to aero center.
 
-        # dmoment__dsec_forces
+        dmom_dsec_in = np.empty((nx - 1, ny - 1, 6))
+        dmom_dsec_in[:, :, 0] = -diff_in[:, :, 2]
+        dmom_dsec_in[:, :, 1] = diff_in[:, :, 1]
+        dmom_dsec_in[:, :, 2] = diff_in[:, :, 2]
+        dmom_dsec_in[:, :, 3] = -diff_in[:, :, 0]
+        dmom_dsec_in[:, :, 4] = -diff_in[:, :, 1]
+        dmom_dsec_in[:, :, 5] = diff_in[:, :, 0]
 
-        dmom_dsec = np.empty((nx - 1, ny - 1, 6))
-        dmom_dsec[:, :, 0] = -diff[:, :, 2]
-        dmom_dsec[:, :, 1] = diff[:, :, 1]
-        dmom_dsec[:, :, 2] = diff[:, :, 2]
-        dmom_dsec[:, :, 3] = -diff[:, :, 0]
-        dmom_dsec[:, :, 4] = -diff[:, :, 1]
-        dmom_dsec[:, :, 5] = diff[:, :, 0]
+        # Repeat for moments at outer node wrt sec_force
+        diff_out = 0.5 * (a_pts - s_pts[1:, :])  # moment arm from outer node to aero center.
+        dmom_dsec_out = np.empty((nx - 1, ny - 1, 6))
+        dmom_dsec_out[:, :, 0] = -diff_out[:, :, 2]
+        dmom_dsec_out[:, :, 1] = diff_out[:, :, 1]
+        dmom_dsec_out[:, :, 2] = diff_out[:, :, 2]
+        dmom_dsec_out[:, :, 3] = -diff_out[:, :, 0]
+        dmom_dsec_out[:, :, 4] = -diff_out[:, :, 1]
+        dmom_dsec_out[:, :, 5] = diff_out[:, :, 0]
 
         id1 = 6 * (ny - 1) * (nx - 1)
         partials['loads', 'sec_forces'][:id1] = 0.5
 
         id2 = id1 * 2
-        dmom_dsec = dmom_dsec.flatten()
-        partials['loads', 'sec_forces'][id1:id2] = dmom_dsec
-        partials['loads', 'sec_forces'][id2:] = dmom_dsec
+        dmom_dsec_in = dmom_dsec_in.flatten()
+        dmom_dsec_out = dmom_dsec_out.flatten()
+        partials['loads', 'sec_forces'][id1:id2] = dmom_dsec_in
+        partials['loads', 'sec_forces'][id2:] = dmom_dsec_out
 
-        # dmoment__dmesh
+        # ----- 2. dmoment__dmesh -----
+        # Sensitivity of moments at inner nodes wrt diff_in (upper diagonal)
+        dmom_ddiff_in = np.zeros((nx - 1, ny - 1, 6))
+        dmom_ddiff_in[:, :, 0] = sec_forces[:, :, 2]
+        dmom_ddiff_in[:, :, 1] = -sec_forces[:, :, 1]
+        dmom_ddiff_in[:, :, 2] = -sec_forces[:, :, 2]
+        dmom_ddiff_in[:, :, 3] = sec_forces[:, :, 0]
+        dmom_ddiff_in[:, :, 4] = sec_forces[:, :, 1]
+        dmom_ddiff_in[:, :, 5] = -sec_forces[:, :, 0]
+        dmom_ddiff_in_sum = np.sum(dmom_ddiff_in, axis=0)
 
-        dmom_ddiff = np.zeros((nx - 1, ny - 1, 6))
-        dmom_ddiff[:, :, 0] = sec_forces[:, :, 2]
-        dmom_ddiff[:, :, 1] = -sec_forces[:, :, 1]
-        dmom_ddiff[:, :, 2] = -sec_forces[:, :, 2]
-        dmom_ddiff[:, :, 3] = sec_forces[:, :, 0]
-        dmom_ddiff[:, :, 4] = sec_forces[:, :, 1]
-        dmom_ddiff[:, :, 5] = -sec_forces[:, :, 0]
-
-        dmom_ddiff_sum = np.sum(dmom_ddiff, axis=0)
+        # Sensitivity of moments at outer nodes wrt diff_out (lower diagonal)
+        dmom_ddiff_out = np.zeros((nx - 1, ny - 1, 6))
+        dmom_ddiff_out[:, :, 0] = sec_forces[:, :, 2]
+        dmom_ddiff_out[:, :, 1] = -sec_forces[:, :, 1]
+        dmom_ddiff_out[:, :, 2] = -sec_forces[:, :, 2]
+        dmom_ddiff_out[:, :, 3] = sec_forces[:, :, 0]
+        dmom_ddiff_out[:, :, 4] = sec_forces[:, :, 1]
+        dmom_ddiff_out[:, :, 5] = -sec_forces[:, :, 0]
+        dmom_ddiff_out_sum = np.sum(dmom_ddiff_out, axis=0)
 
         dmon_ddiff_diag = np.zeros((nx - 1, ny, 6))
-        dmon_ddiff_diag[:, 1:, :] = dmom_ddiff
-        dmon_ddiff_diag[:, :-1, :] += dmom_ddiff
+        dmon_ddiff_diag[:, 1:, :] = dmom_ddiff_out
+        dmon_ddiff_diag[:, :-1, :] += dmom_ddiff_in
         dmon_ddiff_diag_sum = np.zeros((1, ny, 6))
-        dmon_ddiff_diag_sum[:, :-1, :] = dmom_ddiff_sum
-        dmon_ddiff_diag_sum[:, 1:, :] += dmom_ddiff_sum
+        dmon_ddiff_diag_sum[:, :-1, :] = dmom_ddiff_in_sum
+        dmon_ddiff_diag_sum[:, 1:, :] += dmom_ddiff_out_sum
 
-        dmom_ddiff = dmom_ddiff.flatten()
-        dmom_ddiff_sum = dmom_ddiff_sum.flatten()
+        dmom_ddiff_in = dmom_ddiff_in.flatten()
+        dmom_ddiff_out = dmom_ddiff_out.flatten()
         dmon_ddiff_diag = dmon_ddiff_diag.flatten()
         dmon_ddiff_diag_sum = dmon_ddiff_diag_sum.flatten()
 
@@ -228,26 +244,22 @@ class LoadTransfer(om.ExplicitComponent):
         partials['loads', 'def_mesh'][:] = 0.0
 
         # Upper diagonal blocks
-        partials['loads', 'def_mesh'][:idw] = dmom_ddiff * ((1 - w1) * 0.25)
-        partials['loads', 'def_mesh'][idy:idx] += dmom_ddiff * (w1 * 0.25)
-        partials['loads', 'def_mesh'][:idy] -= dmom_ddiff_sum * ((1 - w2) * 0.25)
-        partials['loads', 'def_mesh'][idx - idy : idx] -= dmom_ddiff_sum * (w2 * 0.25)
+        partials['loads', 'def_mesh'][:idw] = dmom_ddiff_in * ((1 - w1) * 0.25)
+        partials['loads', 'def_mesh'][idy:idx] += dmom_ddiff_in * (w1 * 0.25)
 
         # Lower Diagonal blocks
         id2 = idx * 2
-        partials['loads', 'def_mesh'][idx : idx + idw] = dmom_ddiff * ((1 - w1) * 0.25)
-        partials['loads', 'def_mesh'][idx + idy : id2] += dmom_ddiff * (w1 * 0.25)
-        partials['loads', 'def_mesh'][idx : idx + idy] -= dmom_ddiff_sum * ((1 - w2) * 0.25)
-        partials['loads', 'def_mesh'][id2 - idy : id2] -= dmom_ddiff_sum * (w2 * 0.25)
+        partials['loads', 'def_mesh'][idx : idx + idw] = dmom_ddiff_out * ((1 - w1) * 0.25)
+        partials['loads', 'def_mesh'][idx + idy : id2] += dmom_ddiff_out * (w1 * 0.25)
 
         # Central Diagonal blocks
         idy = 6 * ny
         idz = 6 * (nx - 1)
         id3 = id2 + idw + idz
         partials['loads', 'def_mesh'][id2:id3] = dmon_ddiff_diag * ((1 - w1) * 0.25)
-        partials['loads', 'def_mesh'][id2 : id2 + idy] -= dmon_ddiff_diag_sum * ((1 - w2) * 0.25)
+        partials['loads', 'def_mesh'][id2 : id2 + idy] -= dmon_ddiff_diag_sum * ((1 - w2) * 0.5)
 
         id2 += idy
         id3 += idy
         partials['loads', 'def_mesh'][id2:id3] += dmon_ddiff_diag * (w1 * 0.25)
-        partials['loads', 'def_mesh'][id3 - idy : id3] -= dmon_ddiff_diag_sum * (w2 * 0.25)
+        partials['loads', 'def_mesh'][id3 - idy : id3] -= dmon_ddiff_diag_sum * (w2 * 0.5)
