6c40f4db78305afdb9321a78ffaf7bc3ab05fa15
kanekosh <shugok@umich.edu>
2021-08-17 20:16:17 -0400

    black

--- openaerostruct/aerodynamics/tests/test_coeffs.py
+++ openaerostruct/aerodynamics/tests/test_coeffs.py
@@ -5,12 +5,11 @@ from openaerostruct.utils.testing import run_test, get_default_surfaces
 
 
 class Test(unittest.TestCase):
-
     def test(self):
         comp = Coeffs()
 
         run_test(self, comp)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
