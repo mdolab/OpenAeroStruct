b155235da7f6a97e427486ec4645e3b833bd8366
Andrew Lamkin <lamkina@umich.edu>
2022-07-12 11:29:55 -0400

    Fixed flake8 problems

--- openaerostruct/tests/test_aero.py
+++ openaerostruct/tests/test_aero.py
@@ -110,5 +110,6 @@ class Test(unittest.TestCase):
         assert_near_equal(prob["aero_point_0.wing_perf.CL"][0], 0.5, 1e-6)
         assert_near_equal(prob["aero_point_0.CM"][1], -1.7885550372372376, 1e-6)
 
+
 if __name__ == "__main__":
     unittest.main()
