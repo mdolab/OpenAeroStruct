d0cc35ccd7a3198e1fa59fcdc3b5716f47a6bfc0
Eytan Adler <eytana@umich.edu>
2023-03-20 11:27:41 -0400

    Trying something else

--- openaerostruct/utils/vector_algebra.py
+++ openaerostruct/utils/vector_algebra.py
@@ -14,11 +14,9 @@ def compute_dot(array1, array2):
     Parameters
     ----------
     array1 : numpy array[..., 3]
-        First argument in the dot product.
-        The dot product axis is the last one.
+        First argument in the dot product. The dot product axis is the last one.
     array2 : numpy array[..., 3]
-        Second argument in the dot product.
-        The dot product axis is the last one.
+        Second argument in the dot product. The dot product axis is the last one.
     """
     return np.einsum("...,i->...i", np.einsum("...i,...i->...", array1, array2), np.ones(3))
 
@@ -28,11 +26,9 @@ def compute_dot_deriv(array, deriv_array):
     Parameters
     ----------
     array : numpy array[..., 3]
-        The argument in the dot product we are not taking the derivatives for.
-        The dot product axis is the last one.
+        The argument in the dot product we are not taking the derivatives for. The dot product axis is the last one.
     deriv_array : numpy array[..., 3, 3]
-        The derivatives of the argument in the dot product of interest.
-        The dot product axis is the last one.
+        The derivatives of the argument in the dot product of interest. The dot product axis is the last one.
     """
     return np.einsum(
         "...j,i->...ij",
@@ -46,11 +42,9 @@ def compute_cross(array1, array2):
     Parameters
     ----------
     array1 : numpy array[..., 3]
-        First argument in the cross product (order matters).
-        The cross product axis is the last one.
+        First argument in the cross product (order matters). The cross product axis is the last one.
     array2 : numpy array[..., 3]
-        Second argument in the cross product (order matters).
-        The cross product axis is the last one.
+        Second argument in the cross product (order matters). The cross product axis is the last one.
     """
     return np.cross(array1, array2, axis=-1)
 
@@ -60,11 +54,9 @@ def compute_cross_deriv1(deriv_array, array):
     Parameters
     ----------
     deriv_array : numpy array[..., 3, 3]
-        Derivatives of the first argument in the cross product.
-        The cross product axis is the second last one.
+        Derivatives of the first argument in the cross product. The cross product axis is the second last one.
     array : numpy array[..., 3]
-        Second argument in the cross product (order matters).
-        The cross product axis is the last one.
+        Second argument in the cross product (order matters). The cross product axis is the last one.
     """
     tmp_0 = np.einsum("...i,j->...ij", compute_cross(deriv_array[..., 0], array), np.array([1.0, 0.0, 0.0]))
     tmp_1 = np.einsum("...i,j->...ij", compute_cross(deriv_array[..., 1], array), np.array([0.0, 1.0, 0.0]))
@@ -77,11 +69,9 @@ def compute_cross_deriv2(array, deriv_array):
     Parameters
     ----------
     array : numpy array[..., 3]
-        First argument in the cross product (order matters).
-        The cross product axis is the last one.
+        First argument in the cross product (order matters). The cross product axis is the last one.
     deriv_array : numpy array[..., 3, 3]
-        Derivatives of the second argument in the cross product.
-        The cross product axis is the second last one.
+        Derivatives of the second argument in the cross product. The cross product axis is the second last one.
     """
     tmp_0 = np.einsum("...i,j->...ij", compute_cross(array, deriv_array[..., 0]), np.array([1.0, 0.0, 0.0]))
     tmp_1 = np.einsum("...i,j->...ij", compute_cross(array, deriv_array[..., 1]), np.array([0.0, 1.0, 0.0]))
