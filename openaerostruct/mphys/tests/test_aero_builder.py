4bdf65449ec297b033c4621cca8a3857f84eb147
Tim Brooks <41971846+timryanb@users.noreply.github.com>
2023-07-20 11:52:03 -0400

    minor test edit

--- openaerostruct/mphys/tests/test_aero_builder.py
+++ openaerostruct/mphys/tests/test_aero_builder.py
@@ -17,28 +17,30 @@ except ImportError:
 @unittest.skipUnless(mphys_mpi_flag, "mphys/mpi4py is required.")
 class Test(unittest.TestCase):
     def setUp(self):
-        surfaces = get_default_surfaces()
+        self.surfaces = get_default_surfaces()
         comm = MPI.COMM_WORLD
         # Create mphys builder for aero solver
-        self.aero_builder = AeroBuilder(surfaces)
+        self.aero_builder = AeroBuilder(self.surfaces)
         self.aero_builder.initialize(comm)
 
     def test_tagged_indices(self):
+        wing_nnodes = self.surfaces[0]["mesh"].size // 3
+        tail_nnodes = self.surfaces[1]["mesh"].size // 3
         with self.subTest(case="wing"):
             wing_inds = self.aero_builder.get_tagged_indices(["wing"])
-            np.testing.assert_equal(wing_inds, np.arange(0, 8))
+            np.testing.assert_equal(wing_inds, np.arange(0, wing_nnodes))
 
         with self.subTest(case="tail"):
             tail_inds = self.aero_builder.get_tagged_indices(["tail"])
-            np.testing.assert_equal(tail_inds, np.arange(8, 23))
+            np.testing.assert_equal(tail_inds, np.arange(wing_nnodes, wing_nnodes+tail_nnodes))
 
         with self.subTest(case="wing+tail"):
             wt_inds = self.aero_builder.get_tagged_indices(["wing", "tail"])
-            np.testing.assert_equal(wt_inds, np.arange(0, 23))
+            np.testing.assert_equal(wt_inds, np.arange(0, wing_nnodes+tail_nnodes))
 
         with self.subTest(case="all"):
             wt_inds = self.aero_builder.get_tagged_indices(-1)
-            np.testing.assert_equal(wt_inds, np.arange(0, 23))
+            np.testing.assert_equal(wt_inds, np.arange(0, wing_nnodes+tail_nnodes))
 
 
 if __name__ == "__main__":
