340de02a2813ef611e108d21e788e2a7abb14479
Safa <bakhshi@umich.edu>
2024-02-23 15:54:59 -0500

    Complete the initial implementation of the user custom mesh utility

--- openaerostruct/geometry/geometry_user_mesh_util.py
+++ openaerostruct/geometry/geometry_user_mesh_util.py
@@ -1,215 +1,41 @@
+''' Utility for quickly generating a multi-section user specificed OAS mesh 
 '''
-geomEngine3 straight port to Python for OAS
-by Safa Bakhshi
-WIP
-'''
-#For testing
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-'''
-def userGeom4(sections,data,bPanelsL,cPanels,plotCont,symmetry=True):
+
+def generateMesh(sections,data,bPanels,cPanels,symmetry=True,controlPoints=False,generatePlots=False,plotOptions={}):
 	"""
-    Quickly generates user mesh for analysis based on a few parameters
-
-    Parameters
-    ----------
-    sections : int
-        Integer for number of wing section specified
-    data : numpy array
-        sectionsx4 array consisting of taper, root chord length, aspect ratio, and leading edge sweep in column. Each row corresponds to each specified wing section.
-    bPanelsL: numpy array
-        1 x sections 1-D array consisting of integers that specify the number of spanwise panels corresponding to each wing section
-	cPanels: int
-        Integer for number of chord-wise panels
-	symmetry : boolean
-        Flag set to True if surface is reflected about y=0 plane.
-
-    Returns
-    -------
-    mesh[nx, ny, 3] : numpy array
-        Nodal mesh defining the aerodynamic surface.
-    """	
-
-    #Data Initialization
-	if np.shape(data) != (sections,4):
-		raise Exception("Data not specified for every section")
-
-	if len(bPanelsL) != sections:
-		raise Exception("Number of spanwise panels not specified for every section")
-
-
-	tquartX = []
-	panelQC = []
-	panelGY = []
-	panelGX = []
-	K = []
-
-	Stot = 0
-	span = 0
-	Npanels = 2*np.sum(bPanelsL)*cPanels
-
-	for sec in range(sections):
-		# Select panel number set
-		bPanels = bPanelsL[sec]
-
-		if sec == 0:
-			taper = data[sec,0]
-			rootC = data[sec,1]
-			AR = data[sec,2]
-			tipC = rootC*taper
-			S = AR*((tipC+rootC)/2)**2
-			b = np.sqrt(AR*S)
-			leLambda = np.deg2rad(data[sec,3])
-			Stot = Stot + S
-			span = span + b
-			mainrootC = rootC
-			maintipC = tipC
-		else:
-			taper = data[sec,0]
-			rootC = np.abs(panelGX[sec-1][0,0] - panelGX[sec-1][cPanels,0])
-			AR = data[sec,2]
-			tipC = rootC*taper
-			S = AR*((tipC+rootC)/2)**2
-			b = np.sqrt(AR*S)
-			leLambda = np.deg2rad(data[sec,3])
-			Stot = Stot + S
-			span = span + b
-			if sec == sections-1:
-				maintipC = tipC
-
-		CAve = (maintipC + mainrootC)/2
-
-		#Generate geomtery
-		if sec == 0:
-			rootEnd = 0;
-			rootStart = rootC + rootEnd
-
-			tipStart = rootStart - (b/2)*np.tan(leLambda)
-			tipEnd = tipStart - tipC
-		else:
-			rootEnd = panelGX[sec-1][cPanels,0]
-			rootStart = rootC + rootEnd
-
-			tipStart = rootStart - (b/2)*np.tan(leLambda)
-			tipEnd = tipStart - tipC
-
-		#Generate Panels
-		#Descretize wing root and wing tip into N+1 points  
-		#rootChordPoints = np.arange(rootStart,rootEnd-(rootStart-rootEnd)/cPanels,-(rootStart-rootEnd)/cPanels)
-		rootChordPoints = np.linspace(rootStart,rootEnd,cPanels+1)
-
-		if tipStart == tipEnd:
-			tipChordPoints = tipStart*np.ones(len(rootChordPoints))
-		else:
-			tipChordPoints = np.linspace(tipStart,tipEnd,len(rootChordPoints))
-
-		
-		K.append(b/(2*bPanels))
-		if sec == 0:
-			panelGeomY = np.arange(-b/2,b/2+K[sec],K[sec])
-		else:
-			panelGeomY = np.zeros(2*bPanels)
-			panelGeomY[0:bPanels] = np.linspace(panelGY[sec-1][0]-(b/2),panelGY[sec-1][0]-K[sec],bPanels)
-			panelGeomY[bPanels:2*bPanels] = np.linspace(panelGY[sec-1][-1]+K[sec],panelGY[sec-1][-1]+(b/2),bPanels)
-
-		if sec == 0:
-			centreIndex = bPanels;
-		else:
-			centreIndex = bPanels -1;
-
-		panelGeomX = np.zeros([len(rootChordPoints),len(panelGeomY)])
-		if sec == 0:
-			for i in range(len(rootChordPoints)):
-				#Left Wing
-				panelGeomX[i,0:centreIndex+1] = rootChordPoints[i] + ((tipChordPoints[i]-rootChordPoints[i])/(-b/2))*(panelGeomY[0:centreIndex+1])
-				#Right Wing
-				panelGeomX[i,centreIndex+1:] = rootChordPoints[i] + ((tipChordPoints[i]-rootChordPoints[i])/(b/2))*(panelGeomY[centreIndex+1:])
-		else:
-			for i in range(len(rootChordPoints)):
-				#Left Wing
-				panelGeomX[i,0:centreIndex+1] = rootChordPoints[i] + ((tipChordPoints[i]-rootChordPoints[i])/(-b/2))*((panelGeomY[0:centreIndex+1])-panelGY[sec-1][0])
-				#Right Wing
-				panelGeomX[i,centreIndex+1:] = rootChordPoints[i] + ((tipChordPoints[i]-rootChordPoints[i])/(b/2))*(panelGeomY[centreIndex+1:] - panelGY[sec-1][-1])
-		
-		panelQuarterC = np.zeros([cPanels,len(panelGeomY)])
-		tquarterPointsX = np.zeros([cPanels,len(panelGeomY)])
-
-		for i in range(len(panelGeomY)):
-			for j in range(cPanels-1):
-				panelQuarterC[j,i] = panelGeomX[j,i] + (panelGeomX[j+1,i] - panelGeomX[j,i])/4
-				tquarterPointsX[j,i] = panelGeomX[j,i] + 1*(panelGeomX[j+1,i]-panelGeomX[j,i])/2
-
-
-		panelQC.append(panelQuarterC)
-		panelGY.append(panelGeomY)
-		tquartX.append(tquarterPointsX)
-		panelGX.append(panelGeomX)
-
-	#Stitch the results into a mesh
-	for i in range(sections):
-		if i == 0:
-			mainQuarterC = panelQC[i]
-			mainPanelGeomY = panelGY[i]
-			mainPanelGeomX = panelGX[i]
-			maintquarterPointsX = tquartX[i]
-		else:
-			mainQuarterC = np.concatenate((panelQC[i][:,0:bPanelsL[i]],mainQuarterC,panelQC[i][:,bPanelsL[i]:]),axis=1)
-			maintquarterPointsX = np.concatenate((tquartX[i][:,0:bPanelsL[i]],maintquarterPointsX,tquartX[i][:,bPanelsL[i]:]),axis=1)
-			mainPanelGeomY = np.concatenate((panelGY[i][0:bPanelsL[i]],mainPanelGeomY,panelGY[i][bPanelsL[i]:]))
-			mainPanelGeomX = np.concatenate((panelGX[i][:,0:bPanelsL[i]],mainPanelGeomX,panelGX[i][:,bPanelsL[i]:]),axis=1)
-
-	#Use stiched geomtery to calculate control point locations and chord distribution
-
-	mainControlPointsX = np.zeros([cPanels,2*np.sum(bPanelsL)])
-	mainVControlPointsX = np.zeros([cPanels,2*np.sum(bPanelsL)])
-	mainControlPointsY = np.zeros(2*np.sum(bPanelsL))
-	chordDistGeom = np.zeros(len(mainPanelGeomY))
-	chordDistCont = np.zeros(len(mainControlPointsY))
-
-	for i in range(len(mainPanelGeomY)):
-		chordDistGeom[i] = mainPanelGeomX[0,i] - mainPanelGeomX[-1,i]
-
-	for i in range(len(mainPanelGeomY)-1):
-		for j in range(cPanels):
-			mainControlPointsX[j,i] = (maintquarterPointsX[j,i+1]+maintquarterPointsX[j,i])/2
-			mainVControlPointsX[j,i] = (mainQuarterC[j,i+1]+mainQuarterC[j,i])/2
-		mainControlPointsY[i] = mainPanelGeomY[i] + (mainPanelGeomY[i+1]-mainPanelGeomY[i])/2
-		chordDistCont[i] = (chordDistGeom[i+1] + chordDistGeom[i])/2
-
-	#Cut the planform if symmetry is set
-	if symmetry:
-		print(np.sum(bPanelsL))
-		print(mainPanelGeomX.shape)
-		mainPanelGeomX = mainPanelGeomX[:,0:np.sum(bPanelsL)]
-		mainPanelGeomY = mainPanelGeomY[0:np.sum(bPanelsL)]
-	
-	mainPanelGeomY = np.broadcast_to(mainPanelGeomY,(cPanels+1,len(mainPanelGeomY)))
-
-	# Output OAS mesh
-	mesh = np.zeros((cPanels+1,mainPanelGeomY.shape[1],3))
-	mesh[:,:,0] = mainPanelGeomX
-	mesh[:,:,1] = mainPanelGeomY
-	return span, Stot, panelGX, panelGY, mainPanelGeomX, mainPanelGeomY, mesh
-'''
-
-def generateMesh(sections,data,bPanels,cPanels,plotCont,symmetry=True):
-	"""
-    Quickly generates user mesh for analysis based on a few parameters
+    Quickly generates user multi-section mesh for analysis based on a few parameters
 
     Parameters
     ----------
     sections : int
         Integer for number of wing sections specified
     data : numpy array
-        sectionsx4 array consisting of taper, root chord length, aspect ratio, and leading edge sweep in column. Each row corresponds to each specified wing section.
+        sectionsx4 array consisting of taper, root chord length, aspect ratio, and leading edge sweep in columns. Each row corresponds to each specified wing section.
     bPanels: numpy array
         1 x sections 1-D array consisting of integers that specify the number of spanwise panels corresponding to each wing section
 	cPanels: int
         Integer for number of chord-wise panels
 	symmetry : boolean
         Flag set to True if surface is reflected about y=0 plane.
+	controlPoints: boolean
+		Flag set to True if both quarter chord and three-quarter chord control points should be computed 
+	generatePlots: boolean
+		Flag set to True if planform and mesh plots should be created and saved.
+	plotOptions: Dict
+		Dictionary containing user options for plotting
+	 	'type': 'plan', 'mesh', 'all'
+		    Plots the planform geometry only, mesh only, or both
+		'symmetry': 'Left', 'Right', 'Full
+			Plots the left, right, or full spans
+		'name': string
+			
+		
+	
 
     Returns
     -------
@@ -221,15 +47,30 @@ def generateMesh(sections,data,bPanels,cPanels,plotCont,symmetry=True):
 	if np.shape(data) != (sections,4):
 		raise Exception("Data not specified for every section")
 
-	if len(bPanelsL) != sections:
+	if len(bPanels) != sections:
 		raise Exception("Number of spanwise panels not specified for every section")
 	
 	panelGX, panelGY, panelQC, tquartX, Npanels = generateSectionGeometry(sections,data,bPanels,cPanels)
+	panelGeomX, panelGeomY = stitchSectionGeometry(sections,panelGY,panelGX,bPanels)
 
+	if symmetry:
+		panelGeomX, panelGeomY = planformSymmetric(panelGeomX,panelGeomY,bPanels)
+	if controlPoints:
+		quarterC, tquarterPointsX = stitchPanelChordGeometry(sections,panelQC,tquartX,bPanels)
+		controlPointsX, qControlPointsX, controlPointsY, chordDistGeom, chordDistCont = calcControlPoints(panelGeomY,panelGeomX,tquartX,panelQC,bPanels,cPanels)
+	if generatePlots:
+		if plotOptions['type'] == 'plan':
+			plotPlanform(sections,panelGX,panelGY,plotOptions['symmetry'],plotOptions['name'])
+		elif plotOptions['type'] == 'mesh':
+			plotPanels(sections,panelGX,panelGY,plotOptions['symmetry'],plotOptions['name'])
+		else:
+			plotPlanform(sections,panelGX,panelGY,plotOptions['symmetry'],plotOptions['name'])
+			plotPanels(sections,panelGX,panelGY,plotOptions['symmetry'],plotOptions['name'])
 
+	mesh = outputOASMesh(panelGeomX,panelGeomY)
 
-
-
+	return mesh
+												  
 def generateSectionGeometry(sections,data,bPanels,cPanels):
 	"""
     Constructs the multi-section wing geometry specified by the user and generates a mesh for each section.
@@ -279,16 +120,15 @@ def generateSectionGeometry(sections,data,bPanels,cPanels):
 		taper = data[sec,0]
 		AR = data[sec,2]
 		leLambda = np.deg2rad(data[sec,3])
+		
 		if sec == 0:
 			rootC = data[sec,1]
 			mainrootC = rootC
-			maintipC = tipC
 		else:
 			rootC = np.abs(panelGX[sec-1][0,0] - panelGX[sec-1][cPanels,0])
-			if sec == sections-1:
-				maintipC = tipC
 
 		tipC = rootC*taper
+		maintipC = tipC
 		S = AR*((tipC+rootC)/2)**2
 		b = np.sqrt(AR*S)
 		Stot = Stot + S
@@ -535,7 +375,6 @@ def calcControlPoints(panelGeomY,panelGeomX,tquartX,panelQC,bPanels,cPanels):
 
 	return controlPointsX, qControlPointsX, controlPointsY, chordDistGeom, chordDistCont
 
-
 def planformSymmetric(panelGeomX,panelGeomY,bPanels):
 	"""
     Cuts the mesh in half is symmetry conditions are used
@@ -557,13 +396,11 @@ def planformSymmetric(panelGeomX,panelGeomY,bPanels):
 	panelGeomY : numpy array
 		Array of the mesh y-coordinates
     """
-	#print(np.sum(bPanelsL))
-	#print(mainPanelGeomX.shape)
 	panelGeomX = panelGeomX[:,0:np.sum(bPanels)]
 	panelGeomY = panelGeomY[0:np.sum(bPanels)]
 	return panelGeomX, panelGeomY
 
-def outputOASMesh(panelGeomX,panelGeomY,bPanels,cPanels):
+def outputOASMesh(panelGeomX,panelGeomY): 
 	"""
     Outputs the mesh in OAS format
 
@@ -573,10 +410,7 @@ def outputOASMesh(panelGeomX,panelGeomY,bPanels,cPanels):
 	 	Array of the mesh x-coordinates
 	panelGeomY : numpy array
 		Array of the mesh y-coordinates
-    bPanels: numpy array
-        1 x sections 1-D array consisting of integers that specify the number of spanwise panels corresponding to each wing section
-	cPanels: int
-        Integer for number of chord-wise panels
+
 
 
     Returns
@@ -584,45 +418,13 @@ def outputOASMesh(panelGeomX,panelGeomY,bPanels,cPanels):
 	mesh : numpy array
 	 	3-D array with the OAS format mesh
     """	
-	panelGeomY = np.broadcast_to(panelGeomY,(cPanels+1,len(panelGeomY)))
-	mesh = np.zeros((cPanels+1,mainPanelGeomY.shape[1],3))
+	panelGeomY = np.broadcast_to(panelGeomY,(panelGeomX.shape[0],len(panelGeomY)))
+	mesh = np.zeros((panelGeomX.shape[0],panelGeomY.shape[1],3))
 	mesh[:,:,0] = panelGeomX
 	mesh[:,:,1] = panelGeomY
 	return mesh
 
-
-#TEST
-
-sections = 4;
-#data = np.array([[0.648294, .698608, .345206, .205671],[187.23, 121.37, 84.79, 29.27],[.075305,.089833,.573734,3.46289],[84.9578,82.3695,68,40]]);
-data = np.array([[0.648294,187.23,.075305,84.9578],[.698608,121.37,.089833,82.3695],[.345206,84.79,.573734,68],[.205671,29.27,3.46289,40]])
-bPanels = np.array([3,2,8,15]);
-cPanels = 16;
-
-#sections = 2
-#bPanels = np.array([8,8])
-#cPanels = 8
-
-#data = np.array([[0.3,20,9,33]])
-#data = np.array([[0.3,20,9,33],[0.3,20,9,50]])
-[b, Stot, panelGX, panelGY,mainPanelGeomX, mainPanelGeomY, mesh] = userGeom4(sections,data,bPanels,cPanels,'true',True)
-#plt.figure()
-#plt.scatter(mainPanelGeomY.flatten(),mainPanelGeomX.flatten())
-print('The area of this planform is: {}'.format(Stot))
-
-#Dictionary Idea
-Np3SST = {'sections': 4, 
-		  'bPanels': [3,2,8,15], 
-		  'cPanels': 16,
-		  'taper': [0.648294,.698608,.345206,.205671],
-		  'rootC': [187.23,121.37,84.79,29.27],
-		  'LEsweep': [84.9578,82.3695,68,40],
-		  'symmetry': True,
-		  'plotSymmetry': 'Left'}
-
-
-
-def plotPlanform(sections,panelGX,panelGY,plotSymmetry='Left'):
+def plotPlanform(sections,panelGX,panelGY,plotSymmetry='Left',wingName='CustomUserWing'):
 	"""
     Plots the outline of all definied planform sections.
 
@@ -637,6 +439,9 @@ def plotPlanform(sections,panelGX,panelGY,plotSymmetry='Left'):
 	plotSymmetry : string
         Flag set to 'Left' if only y<=0 plotted
         Flag set to 'Right' if only y>=0 plotted
+		Flag set to 'None' if full planform plotted
+	wingName: string
+		Name for plot title
 
     Returns
     -------
@@ -675,11 +480,13 @@ def plotPlanform(sections,panelGX,panelGY,plotSymmetry='Left'):
 			else:
 				plt.plot([ panelGY[i-1][0],panelGY[i-1][0],panelGY[i][0],panelGY[i][0],panelGY[i-1][0] ],[ rootStart,rootEnd,panelGX[i][cPanels,0],panelGX[i][0,0],rootStart ],c=colorSet[i,:])
 				plt.plot([ panelGY[i-1][-1],panelGY[i][-1],panelGY[i][-1],panelGY[i-1][-1],panelGY[i-1][-1] ],[ rootEnd,panelGX[i][cPanels,0],panelGX[i][0,0],rootStart, rootStart ],c=colorSet[i,:])
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.title('{} Planform Geometry'.format(wingName))
+	plt.savefig('{}PlanformGeom.pdf'.format(wingName))
 
-plotPlanform(sections,panelGX,panelGY,'Left')
 
-
-def plotPanels(sections,panelGX,panelGY,plotSymmetry='Left'):
+def plotPanels(sections,panelGX,panelGY,plotSymmetry='Left',wingName='CustomUserWing'):
 	"""
     Plots the mesh(panels) of each planform section
 
@@ -694,6 +501,9 @@ def plotPanels(sections,panelGX,panelGY,plotSymmetry='Left'):
 	symmetry : string
         Flag set to 'Left' if only y<=0 plotted
         Flag set to 'Right' if only y>=0 plotted
+		Flag set to 'None' if full planform plotted
+	wingName: string
+		Name for plot title
 
     Returns
     -------
@@ -735,8 +545,53 @@ def plotPanels(sections,panelGX,panelGY,plotSymmetry='Left'):
 					plt.plot([panelGY[i-1][-1],panelGY[i][-1]],[panelGX[i-1][j,0],panelGX[i][j,0]],c=colorSet[i,:])
 					for k in range(len(panelGY[i])):
 						plt.plot([panelGY[i][k],panelGY[i][k]],[panelGX[i][0,k],panelGX[i][-1,k]],c=colorSet[i,:])
+	plt.xlabel('x')
+	plt.ylabel('y')
+	plt.title('{} Mesh'.format(wingName))
+	plt.savefig('{}Mesh.pdf'.format(wingName))
 
 
-plotPanels(sections,panelGX,panelGY,'Left')
+if __name__ == "__main__":
+	'''Generate example meshes'''
 
-plt.show()
+	plotOptions = {}
+	plotOptions['type'] = 'all'
+	plotOptions['symmetry'] = 'Left'
+	plotOptions['name'] = 'Rectangular Wing'
+
+	#Simple rectangular wing
+	# 32 x 16 mesh
+	sections = 1
+	data = np.array([[1,1,10,0]])
+	bPanels = np.array([16])
+	cPanels = 16
+	mesh = generateMesh(sections,data,bPanels,cPanels,True,False,True,plotOptions)
+
+
+	#Double Delta Wing 75/65
+	plotOptions['name'] = 'Double Delta Wing 75\\65'
+
+
+	sections = 2
+	bPanels = np.array([6,7])
+	cPanels = 16
+	data = np.array([[0.39319,14.4533,.466822,75],[0,5.6829,1.86524,65]])
+	mesh = generateMesh(sections,data,bPanels,cPanels,True,False,True,plotOptions)
+
+	
+
+
+	#NASA N+3 SST 
+	plotOptions['name'] = 'NASA N+3 SST'
+	sections = 4
+	data = np.array([[0.648294,187.23,.075305,84.9578],[.698608,121.37,.089833,82.3695],[.345206,84.79,.573734,68],[.205671,29.27,3.46289,40]])
+	bPanels = np.array([3,2,8,15])
+	cPanels = 16
+
+
+	mesh = generateMesh(sections,data,bPanels,cPanels,True,False,True,plotOptions)
+	plt.show()
+
+
+
+
