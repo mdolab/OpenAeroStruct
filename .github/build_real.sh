0eda368c69e7009e2a6d29861bcdb98bcdd9b201
Shugo Kaneko <49300827+kanekosh@users.noreply.github.com>
2024-12-16 20:36:11 +0900

    Removed openmdao version constraint

--- .github/build_real.sh
+++ .github/build_real.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 sed -i '/mphys/d' "$HOME/.config/pip/constraints.txt" # Remove the pip constraint on the mphys version
+sed -i '/openmdao/d' "$HOME/.config/pip/constraints.txt" # Remove the pip constraint on the openmdao version
 pip install .[testing,mphys]
