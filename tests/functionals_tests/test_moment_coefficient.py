e298445453df816bf84b6d2587e4904c824f7527
Safa <bakhshi@umich.edu>
2024-12-16 16:31:06 -0500

    merged tests

--- tests/functionals_tests/test_moment_coefficient.py
+++ tests/functionals_tests/test_moment_coefficient.py
@@ -28,28 +28,12 @@ class Test(unittest.TestCase):
         indep_var_comp = om.IndepVarComp()
 
         indep_var_comp.add_output("S_ref_total", val=1e4, units="m**2")
-
-        group.add_subsystem("moment_calc", comp)
-        group.add_subsystem("indep_var_comp", indep_var_comp)
-
-        group.connect("indep_var_comp.S_ref_total", "moment_calc.S_ref_total")
-
-        run_test(self, group, complex_flag=True, method="cs")
-
-    def test3(self):
-        surfaces = get_default_surfaces()
-
-        group = om.Group()
-
-        comp = MomentCoefficient(surfaces=surfaces)
-
-        indep_var_comp = om.IndepVarComp()
-
         indep_var_comp.add_output("cg", val=np.array([-10.0, 10.0, -10.0]), units="m")
 
         group.add_subsystem("moment_calc", comp)
         group.add_subsystem("indep_var_comp", indep_var_comp)
 
+        group.connect("indep_var_comp.S_ref_total", "moment_calc.S_ref_total")
         group.connect("indep_var_comp.cg", "moment_calc.cg")
 
         run_test(self, group, complex_flag=True, method="cs")
